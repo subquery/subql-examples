68dc0b3e6b2b3feef2c087cf79d9343c9a925222
Ian He <ian@onfinality.io>
2021-01-12 00:03:21 +1300

    example for kitty nft chain

---
+++ kitty/subquery/README.md
@@ -0,0 +1,5 @@
+# SubQuery Example - Timestamp of Blocks
+
+This subquery indexes timestamp of each finalized blocks.
+
+It is an example of CallHandler, by processing `timestamp.set` extrinsic, and extracting the first args of it, we can retrieve the timestamp we want. 
---
+++ kitty/subquery/src/mappings/Kitty.ts
@@ -0,0 +1,31 @@
+import {KittyBirthInfo} from '../types/models/KittyBirthInfo';
+import {SubstrateEvent, SubstrateExtrinsic} from "@subql/types";
+
+export async function handleKittyCreated(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [owner, kittyId, kitty]}} = event;
+    const record = new KittyBirthInfo(kittyId.toString());
+    record.birthBlockHeight = event.extrinsic.block.block.header.number.toBigInt();
+    record.firstOwner = owner.toString();
+    record.owner = owner.toString();
+    await record.save();
+}
+
+export async function handleKittyBred(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const bredEvent = extrinsic.events.find(e => e.event.section === 'kitty' && e.event.method === 'KittyBred');
+    const {event: {data: [owner, kittyId, kitty]}} = bredEvent;
+    const record = new KittyBirthInfo(kittyId.toString());
+    record.birthBlockHeight = extrinsic.block.block.header.number.toBigInt();
+    record.firstOwner = owner.toString();
+    record.owner = owner.toString();
+    const {extrinsic: {method: {args: [kittyId1, kittyId2]}}} = extrinsic;
+    record.parent1 = kittyId1.toString();
+    record.parent2 = kittyId2.toString();
+    await record.save();
+}
+
+export async function handleKittyTransfer(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [from, to, kittyId]}} = event;
+    const record = await KittyBirthInfo.get(kittyId.toString());
+    record.owner = to.toString();
+    await record.save();
+}
