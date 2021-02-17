cf50178012f463d509a249ab146a22b7bfab4fdd
Ian He <ian@onfinality.io>
2021-02-18 11:35:37 +1300

    fix null exception when indexing kusama

--- sum-reward/src/mappings/Reward.ts
+++ sum-reward/src/mappings/Reward.ts
@@ -1,23 +1,36 @@
 import {SumReward} from '../types/models/SumReward';
 import {SubstrateEvent} from "@subql/types";
 import {Balance} from '@polkadot/types/interfaces';
+import {NoBondRecordAccount} from "../types/models/NoBondRecordAccount";
+
+function createSumReward(accountId: string): SumReward {
+    const entity = new SumReward(accountId);
+    entity.accountReward = BigInt(0);
+    entity.accountSlash = BigInt(0);
+    entity.accountTotal = BigInt(0);
+    return entity;
+}
 
 export async function handleBond(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, balance]}} = event;
     const entity = await SumReward.get(account.toString());
     if (entity === undefined){
-        const entity = new SumReward(account.toString());
-        entity.accountReward = BigInt(0);
-        entity.accountSlash = BigInt(0);
-        entity.accountTotal = BigInt(0);
-        await entity.save();
+        await createSumReward(account.toString()).save();
     }
 }
 
 
 export async function handleReward(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newReward]}} = event;
-    const entity = await SumReward.get(account.toString());
+    let entity = await SumReward.get(account.toString());
+    if (entity === undefined){
+        // in early stage of kusama, some validators didn't need to bond to start staking
+        // to not break our code, we will create a SumReward record for them and log them in NoBondRecordAccount
+        entity = createSumReward(account.toString());
+        const errorRecord = new NoBondRecordAccount(account.toString());
+        errorRecord.firstRewardAt = event.block.block.header.number.toNumber();
+        await errorRecord.save();
+    }
 
     entity.accountReward = entity.accountReward + (newReward as Balance).toBigInt();
     entity.accountTotal = entity.accountReward - entity.accountSlash;
