1b8b2af49ea7cf055b226e24e2604cca87850653
JQQQ <jiqiang90@hotmail.com>
2021-03-18 09:59:39 +1300

    Change flatten depth to 1, apply prettier

--- entity-relation/src/mappings/Calls.ts
+++ entity-relation/src/mappings/Calls.ts
@@ -1,34 +1,41 @@
-import {SubstrateExtrinsic} from "@subql/types";
-import {Call} from "../types/models/Call";
-import {Vec} from "@polkadot/types"
-import {AnyTuple, CallBase} from "@polkadot/types/types"
-import {flatDeep} from "./utils/flatten";
+import { SubstrateExtrinsic } from '@subql/types';
+import { Call } from '../types/models/Call';
+import { Vec } from '@polkadot/types';
+import { AnyTuple, CallBase } from '@polkadot/types/types';
+import { flatDeep } from './utils/flatten';
 
-function extractCalls(call: CallBase<AnyTuple>, id: number, parentCallId:string,isExtrinsic:boolean): Call[]{
-    const callId = `${parentCallId}-${id}`
+function extractCalls(
+    call: CallBase<AnyTuple>,
+    id: number,
+    parentCallId: string,
+    isExtrinsic: boolean,
+): Call[] {
+    const callId = `${parentCallId}-${id}`;
     const entity = new Call(callId);
     const entities = [] as Call[];
-    entity.method = call.method
+    entity.method = call.method;
     entity.module = call.section;
-    entity.parentCallId = isExtrinsic? null: parentCallId;
+    entity.parentCallId = isExtrinsic ? null : parentCallId;
     entities.push(entity);
-    if (call.method == 'batchAll' && call.section == 'utility'){
-        const calls = call.args[0] as Vec<CallBase<AnyTuple>>
-        return entities.concat(flatDeep((calls.map((call,idx) =>
-            extractCalls(call,idx,callId,false)
-        )),Infinity));
-    }else{
+    if (call.method == 'batchAll' && call.section == 'utility') {
+        const calls = call.args[0] as Vec<CallBase<AnyTuple>>;
+        return entities.concat(
+            flatDeep(
+                calls.map((call, idx) => extractCalls(call, idx, callId, false)),
+                1,
+            ),
+        );
+    } else {
         return entities;
     }
 }
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void>{
-    const calls =  extractCalls(extrinsic.extrinsic.method,extrinsic.idx,extrinsic.block.block.header.number.toString(),true)
-    await Promise.all(calls.map((call) => call.save()))
-
+export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const calls = extractCalls(
+        extrinsic.extrinsic.method,
+        extrinsic.idx,
+        extrinsic.block.block.header.number.toString(),
+        true,
+    );
+    await Promise.all(calls.map((call) => call.save()));
 }
-
-
-
-
-
