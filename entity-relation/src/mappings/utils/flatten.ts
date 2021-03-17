3af0ebf5ecc6b8dc51ac22f0d6963f93b2c8c51d
JQQQ <jiqiang90@hotmail.com>
2021-03-18 10:00:49 +1300

    Apply prettier

--- entity-relation/src/mappings/utils/flatten.ts
+++ entity-relation/src/mappings/utils/flatten.ts
@@ -1,5 +1,9 @@
-
 export function flatDeep(arr, d = 1) {
-    return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
+    return d > 0
+        ? arr.reduce(
+            (acc, val) =>
+                acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val),
+            [],
+        )
         : arr.slice();
-};
+}
