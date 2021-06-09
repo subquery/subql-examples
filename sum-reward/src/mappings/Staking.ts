28c19dd4390eb78d488a1e6eb6a95c5ba17e6e7a
JQQQ <jiqiang90@hotmail.com>
2021-06-09 16:12:47 +1200

    Add index for account

--- sum-reward/src/mappings/Staking.ts
+++ sum-reward/src/mappings/Staking.ts
@@ -6,7 +6,7 @@ import {Balance} from '@polkadot/types/interfaces';
 export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newReward]}} = event;
     const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
-    entity.address = account.toString();
+    entity.accountId = account.toString();
     entity.balance = (newReward as Balance).toBigInt();
     entity.date = event.block.timestamp;
     await entity.save();
@@ -15,7 +15,7 @@ export async function handleStakingReward(event: SubstrateEvent): Promise<void> 
 export async function handleStakingSlash(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newSlash]}} = event;
     const entity = new StakingSlash(`${event.block.block.header.number}-${event.idx.toString()}`);
-    entity.address = account.toString();
+    entity.accountId = account.toString();
     entity.balance = (newSlash as Balance).toBigInt();
     entity.date = event.block.timestamp;
     await entity.save();
