87895478a8dfc30a27a958ef02f3eaffff46324a
Ruslan <emkil.russel@gmail.com>
2021-08-26 12:30:15 +0300

    handle new event naming (#31)

--- sum-reward/src/mappings/Staking.ts
+++ sum-reward/src/mappings/Staking.ts
@@ -2,6 +2,9 @@ import {StakingReward, StakingSlash} from '../types/models';
 import {SubstrateEvent} from "@subql/types";
 import {Balance} from '@polkadot/types/interfaces';
 
+export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
+    await handleStakingReward(event)
+}
 
 export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newReward]}} = event;
@@ -10,6 +13,10 @@ export async function handleStakingReward(event: SubstrateEvent): Promise<void> 
     entity.balance = (newReward as Balance).toBigInt();
     entity.date = event.block.timestamp;
     await entity.save();
+}
+
+export async function handleStakingSlashed(event: SubstrateEvent): Promise<void> {
+    await handleStakingSlash(event)
 }
 
 export async function handleStakingSlash(event: SubstrateEvent): Promise<void> {
